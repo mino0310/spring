cc2771836a550ad649a95a08482c891a0f8d0037
unknown <bewriter310@gmail.com>
2021-06-07 13:22:00 +0900

    java study

---
+++ src/main/java/hello/core/order/OrderServiceimpl.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceimpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // final 은 무조건 값이 할당되어야 하니 삭제
+    private DiscountPolicy discountPolicy;
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
