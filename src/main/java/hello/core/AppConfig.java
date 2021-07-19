ff86e8757bf1f1e1a5576773162eacc2d3493fcc
minhkim310 <bewriter310@gmail.com>
2021-07-19 18:25:45 +0900

    basePackage

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AgppConfig {
+public class AppConfig {
 
     // @Bean memberService -> new MemoryMemberRepository()
     // @Bean orderService -> new MemoryMemberRepository()
