0b32108f26adee78f388062ce166bd05eef43aa9
mino0310 <bewriter310@gmail.com>
2021-12-26 11:31:44 +0900

    test code 수정

--- spring_basic/src/test/java/hello/core/autowired/AllBeanTest.java
+++ spring_basic/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -7,14 +7,17 @@ import hello.core.member.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 
+//@SpringBootTest
 public class AllBeanTest {
 
     @Test
