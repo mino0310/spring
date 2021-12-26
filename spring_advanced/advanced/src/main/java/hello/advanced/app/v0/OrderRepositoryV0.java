aa72c3f66d922a3b82b25a6f0a15ad21113d01d3
mino0310 <bewriter310@gmail.com>
2021-12-26 17:16:46 +0900

    예제 프로젝트 만들기 - V0

---
+++ spring_advanced/advanced/src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,25 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        // 저장 로지이이익
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외가 발생해쯥니댜");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
