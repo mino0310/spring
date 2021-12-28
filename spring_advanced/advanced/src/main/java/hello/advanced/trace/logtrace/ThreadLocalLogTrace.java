1a27afdb3c32d7c325b6b50aba9c4ead28591c38
mino0310 <bewriter310@gmail.com>
2021-12-28 19:48:16 +0900

    ThreadLocal 동기화 - 개발

---
+++ spring_advanced/advanced/src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
@@ -0,0 +1,76 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ThreadLocalLogTrace implements LogTrace{
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    //    private TraceId traceIdHolder; // traceId 동기화, 동시성 이슈가 발생할 수 있음. 뒤에서 논의함.
+    private ThreadLocal<TraceId> traceIdHolder = new ThreadLocal<>();
+
+    @Override
+    public TraceStatus begin(String message) {
+        syncTraceId();
+        TraceId traceId = traceIdHolder.get();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    @Override
+    public void end(TraceStatus status) {
+        conplete(status, null);
+    }
+
+    @Override
+    public void exception(TraceStatus status, Exception e) {
+        conplete(status, e);
+    }
+
+    private void conplete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+        }
+
+        releaseTraceId();
+    }
+
+    private void syncTraceId() {
+        TraceId traceId = traceIdHolder.get();
+        if (traceId == null) {
+            traceIdHolder.set(new TraceId());
+        } else {
+            traceIdHolder.set(traceId.createNextId());
+        }
+    }
+
+    private void releaseTraceId() {
+        TraceId traceId = traceIdHolder.get();
+        if (traceId.isFirstLevel()) {
+            traceIdHolder.remove(); // destroy
+        } else {
+            traceIdHolder.set(traceId.createPreviousId());
+        }
+    }
+
+    private static String addSpace(String prefix, int level) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + prefix : "|   ");
+        }
+        return sb.toString();
+    }
+
+}
