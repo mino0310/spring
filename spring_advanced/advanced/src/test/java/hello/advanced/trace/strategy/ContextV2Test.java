685aaf08aa076f3f2889b9420046775e13e3aa07
mino0310 <bewriter310@gmail.com>
2021-12-29 15:12:38 +0900

    전략 패턴 - 예제 3

---
+++ spring_advanced/advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,53 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+    /**
+     * 전략 패턴, 익명 내부 클래스
+     * 익명 내부 클래스는 내가 execute 안에서 실행할 코드 조각을 넘긴다고 생각하면 받아들이기 쉬움
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        });
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        });
+    }
+
+    /**
+     * 전략 패턴, 익명 내부 클래스, 람다
+     */
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직 1 실행"));
+        context.execute(() -> log.info("비즈니스 로직 2 실행"));
+    }
+
+}
